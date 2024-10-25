f49d73090980922b39157bbd7ba06662c7cdca45
Lucas Bonny <lucasbonnyb8@gmail.com>
2024-10-25 11:09:27 -0300

    Service layer, component registration

---
+++ src/main/java/br/com/gunthercloud/curso/services/UserService.java
@@ -0,0 +1,25 @@
+package br.com.gunthercloud.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.gunthercloud.curso.entites.User;
+import br.com.gunthercloud.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
